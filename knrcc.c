b17124fa7617563367592ef9c5306f97c29453cf
AKINORI Ishii <aknorsh@gmail.com>
2020-04-24 08:57:22 +0900

    Enable him to process cmp

--- knrcc.c
+++ knrcc.c
@@ -158,8 +158,6 @@ typedef enum {
   ND_DIV, // /
   ND_LT,  // <
   ND_LE,  // <=
-  ND_GT,  // >
-  ND_GE,  // >=
   ND_EQ,  // ==
   ND_NEQ, // !=
   ND_NUM, // Decimal Number
@@ -227,9 +225,9 @@ Node *relational() {
     else if (consume("<="))
       node = new_node(ND_LE, node, add());
     else if (consume(">"))
-      node = new_node(ND_GT, node, add());
+      node = new_node(ND_LT, add(), node);
     else if (consume(">="))
-      node = new_node(ND_GE, node, add());
+      node = new_node(ND_LE, add(), node);
     else
       return node;
   }
@@ -294,22 +292,56 @@ void gen(Node *node) {
   gen(node->lhs);
   gen(node->rhs);
 
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
 
   switch (node->kind) {
     case ND_ADD:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
       printf("  add rax, rdi\n");
       break;
     case ND_SUB:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
       printf("  sub rax, rdi\n");
       break;
     case ND_MUL:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
       printf("  imul rax, rdi\n");
       break;
     case ND_DIV:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
       printf("  cqo\n");
       printf("  idiv rdi\n");
+      break;
+    case ND_EQ:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
+      printf("  cmp rax, rdi\n");
+      printf("  sete al\n");
+      printf("  movzb rax, al\n");
+      break;
+    case ND_NEQ:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
+      printf("  cmp rax, rdi\n");
+      printf("  setne al\n");
+      printf("  movzb rax, al\n");
+      break;
+    case ND_LT:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
+      printf("  cmp rax, rdi\n");
+      printf("  setl al\n");
+      printf("  movzb rax, al\n");
+      break;
+    case ND_LE:
+      printf("  pop rdi\n");
+      printf("  pop rax\n");
+      printf("  cmp rax, rdi\n");
+      printf("  setle al\n");
+      printf("  movzb rax, al\n");
       break;
   }
 
